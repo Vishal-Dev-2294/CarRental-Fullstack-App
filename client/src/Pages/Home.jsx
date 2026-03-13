1768d433c56125a87570414d930979168f2d9848
Vishal-Dev-2294 <vishalkushwaha2294@gmail.com>
2026-03-13 22:34:35 +0530

    project completed

---
+++ client/src/Pages/Home.jsx
@@ -0,0 +1,19 @@
+import Banner from "../Components/Banner";
+import FeaturedSection from "../Components/FeaturedSection";
+import Hero from "../Components/Hero";
+import Newsletter from "../Components/Newsletter";
+import Testimonial from "../Components/Testimonial";
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <FeaturedSection />
+      <Banner />
+      <Testimonial />
+      <Newsletter />
+    </>
+  );
+};
+
+export default Home;
